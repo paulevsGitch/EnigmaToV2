2f890a6355abf2e1595fd09e3b647d0d42aeb556
paulevsGitch <paulevs@yandex.ru>
2024-05-05 16:58:56 +0300

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # EnigmaToV2
 Tool to convert Enigma mappings to V2 mappings
+
+Sources:
+- Enigma Format: https://fabricmc.net/wiki/documentation:enigma_mappings
+- Tiny V2 Format: https://github.com/FabricMC/tiny-remapper/issues/9
+
+### How to use:
+``java -jar enigmatov2.jar <source_folder> <result_folder>``
---
+++ src/main/java/paulevs/enigmatov2/Main.java
@@ -0,0 +1,204 @@
+package paulevs.enigmatov2;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		File dirIn = new File(args[0]);
+		File dirOut = new File(args[1]);
+		Map<String, ClassMapping> mappings = new HashMap<>();
+		readMappings(dirIn, mappings);
+		StringBuilder builder = new StringBuilder("tiny\t2\t0\tintermediary\tnamed\n");
+		mappings.values().forEach(builder::append);
+		FileWriter writer = new FileWriter(dirOut);
+		writer.write(builder.toString());
+		writer.flush();
+		writer.close();
+	}
+	
+	private static void readMappings(File dir, Map<String, ClassMapping> mappings) {
+		for (File file : dir.listFiles()) {
+			if (file.isDirectory()) readMappings(file, mappings);
+			else if (file.getName().endsWith(".mapping")) {
+				ClassMapping c = readMappingFile(file);
+				ClassMapping c2 = mappings.get(c.className);
+				if (c2 != null) c = mergeClasses(c, c2);
+				mappings.put(c.className, c);
+			}
+		}
+	}
+	
+	private static ClassMapping readMappingFile(File file) {
+		List<String> lines;
+		try {
+			lines = Files.readAllLines(file.toPath());
+		}
+		catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		return getMapping(lines, new AtomicInteger());
+	}
+	
+	private static ClassMapping getMapping(List<String> lines, AtomicInteger index) {
+		String line = lines.get(index.getAndIncrement());
+		int startingTabs = getTabs(line);
+		String[] parts = line.trim().split(" ");
+		
+		String className = parts.length > 2 ? parts[2] : parts[1];
+		ClassMapping mapping = new ClassMapping(parts[1], className);
+		MethodMapping activeMethod = null;
+		
+		while (index.get() < lines.size()) {
+			line = lines.get(index.get());
+			
+			int tabs = getTabs(line);
+			if (tabs <= startingTabs) return mapping;
+			
+			line = line.trim();
+			parts = line.split(" ");
+			
+			switch (parts[0]) {
+				case "CLASS" -> {
+					ClassMapping nestedClass = getMapping(lines, index);
+					ClassMapping contained = mapping.nested.get(parts[1]);
+					if (contained != null) {
+						nestedClass = mergeClasses(contained, nestedClass);
+					}
+					mapping.nested.put(parts[1], nestedClass);
+				}
+				case "FIELD" -> mapping.fieldMappings.put(parts[1], "\tf\t" + parts[3] + "\t" + parts[1] + "\t" + parts[2]);
+				case "METHOD" -> {
+					if (parts.length > 3) {
+						activeMethod = new MethodMapping(parts[1],
+							"\tm\t" + parts[3] + "\t" + parts[1] + "\t" + parts[2]
+						);
+						String name = parts[1].startsWith("method_") ? parts[1] : parts[1] + " " + line;
+						mapping.methodsMappings.put(name, activeMethod);
+					}
+				}
+				case "ARG" -> {
+					if (activeMethod != null) {
+						activeMethod.args.put(Integer.parseInt(parts[1]), "\t\tp\t" + parts[1] + "\t" + parts[2]);
+					}
+				}
+			}
+			
+			index.incrementAndGet();
+		}
+		
+		return mapping;
+	}
+	
+	private static ClassMapping mergeClasses(ClassMapping a, ClassMapping b) {
+		String className = a.classMapping.equals(a.className) ? b.classMapping : a.classMapping;
+		ClassMapping result = new ClassMapping(a.className, className);
+		
+		mergeMaps(a.nested, b.nested, result.nested);
+		mergeMaps(a.fieldMappings, b.fieldMappings, result.fieldMappings);
+		
+		a.methodsMappings.forEach((name, mapping1) -> {
+			MethodMapping mapping2 = b.methodsMappings.get(name);
+			if (mapping2 != null) {
+				MethodMapping resultMapping = new MethodMapping(mapping1.methodName, mapping1.methodString);
+				mergeMaps(mapping1.args, mapping2.args, resultMapping.args);
+				result.methodsMappings.put(name, resultMapping);
+			}
+		});
+		mergeMaps(a.methodsMappings, b.methodsMappings, result.methodsMappings);
+		
+		return result;
+	}
+	
+	private static <K, V> void mergeMaps(Map<K, V> a, Map<K, V> b, Map<K, V> out) {
+		a.forEach((k, v) -> { if (!out.containsKey(k)) out.put(k, v); });
+		b.forEach((k, v) -> { if (!out.containsKey(k)) out.put(k, v); });
+	}
+	
+	private static int getTabs(String line) {
+		int count = 0;
+		while (count < line.length() && line.charAt(count) == '\t') count++;
+		return count;
+	}
+	
+	private static void addTabs(StringBuilder builder, int count) {
+		builder.append("\t".repeat(Math.max(0, count)));
+	}
+	
+	private static class ClassMapping {
+		final String className;
+		final String classMapping;
+		
+		final Map<String, String> fieldMappings = new HashMap<>();
+		final Map<String, MethodMapping> methodsMappings = new HashMap<>();
+		final Map<String, ClassMapping> nested = new HashMap<>();
+		
+		ClassMapping(String className, String classMapping) {
+			this.className = className;
+			this.classMapping = classMapping;
+		}
+		
+		String getFileName() {
+			return classMapping.replace("/.", "/") + ".mapping";
+		}
+		
+		@Override
+		public String toString() {
+			StringBuilder builder = new StringBuilder();
+			builder.append("c\t");
+			builder.append(className);
+			if (!classMapping.substring(classMapping.lastIndexOf('/') + 1).startsWith("class_")) {
+				builder.append('\t');
+				builder.append(classMapping);
+			}
+			builder.append('\n');
+			
+			fieldMappings.values().stream().sorted().forEach(field -> {
+				builder.append(field);
+				builder.append('\n');
+			});
+			
+			methodsMappings
+				.values()
+				.stream()
+				.sorted(Comparator.comparing(m -> m.methodName))
+				.forEach(m -> builder.append(m.toString()));
+			
+			nested.values().stream().sorted(Comparator.comparing(c -> c.className)).forEach(child ->
+				builder.append(child.toString())
+			);
+			
+			return builder.toString();
+		}
+	}
+	
+	private static class MethodMapping {
+		final String methodName;
+		final String methodString;
+		final Map<Integer, String> args = new HashMap<>();
+		
+		MethodMapping(String methodName, String methodString) {
+			this.methodName = methodName;
+			this.methodString = methodString;
+		}
+		
+		@Override
+		public String toString() {
+			final StringBuilder builder = new StringBuilder(methodString);
+			builder.append('\n');
+			args.keySet().stream().sorted().forEach(key -> {
+				builder.append(args.get(key));
+				builder.append('\n');
+			});
+			return builder.toString();
+		}
+	}
+}
